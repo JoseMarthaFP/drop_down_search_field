8ce947bf2d16de2573171d37b7ecc6ad1e4aef13
José Martha <jose.martha@fashionphile.com>
2025-05-08 18:46:26 -0600

    Added constructor to suggestions model

--- lib/src/suggestions/suggestions_model.dart
+++ lib/src/suggestions/suggestions_model.dart
@@ -1,4 +1,45 @@
+import 'dart:convert';
+
 class SuggestionsModel {
-  dynamic data;
-  bool willCloseSuggestionBox = true;
+  final dynamic data;
+  final bool willCloseSuggestionBox;
+  SuggestionsModel({
+    required this.data,
+    this.willCloseSuggestionBox = true,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'data': data,
+      'willCloseSuggestionBox': willCloseSuggestionBox,
+    };
+  }
+
+  factory SuggestionsModel.fromMap(Map<String, dynamic> map) {
+    return SuggestionsModel(
+      data: map['data'],
+      willCloseSuggestionBox: map['willCloseSuggestionBox'] ?? false,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory SuggestionsModel.fromJson(String source) =>
+      SuggestionsModel.fromMap(json.decode(source));
+
+  @override
+  String toString() =>
+      'SuggestionsModel(data: $data, willCloseSuggestionBox: $willCloseSuggestionBox)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is SuggestionsModel &&
+        other.data == data &&
+        other.willCloseSuggestionBox == willCloseSuggestionBox;
+  }
+
+  @override
+  int get hashCode => data.hashCode ^ willCloseSuggestionBox.hashCode;
 }
